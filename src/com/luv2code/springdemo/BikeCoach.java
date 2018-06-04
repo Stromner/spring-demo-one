e4b07102a3ec74c2de572560497738d05faa749f
David Stromner <davidstromner@gmail.com>
2018-06-04 15:57:41 +0200

    Dependencay injection added

--- src/com/luv2code/springdemo/BikeCoach.java
+++ src/com/luv2code/springdemo/BikeCoach.java
@@ -1,8 +1,20 @@
 package com.luv2code.springdemo;
 
 public class BikeCoach implements Coach{
+	
+	private FortuneService fortuneService;
+	
+	public BikeCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Bike 40k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 }
