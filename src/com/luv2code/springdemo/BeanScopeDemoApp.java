10ee86ba830fa111ff00e70beb91f99b8790a7a8
David Stromner <davidstromner@gmail.com>
2018-06-07 16:09:54 +0200

    Bean scope added/tested

---
+++ src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		// See if it's the same reference
+		System.out.println("theCoach==alphaCoach? " + (theCoach==alphaCoach));
+		System.out.println("theCoach ref:\t" + theCoach);
+		System.out.println("alphaCoach ref:\t" + alphaCoach);
+		
+		context.close();
+	}
+
+}
